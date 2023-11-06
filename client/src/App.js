edfcaf0371ba0b469698fddc2b3270f020ffefdc
Kevtom2003 <kevtom2003@gmail.com>
2023-11-06 17:55:22 -0600

    set up chakra

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Login from "./components/Login/Login";
 import ManagerDashboard from "./components/ManagerDashboard/ManagerDashboard";
+import MenuBoard from "./components/MenuBoard/menuboard";
 // import "./App.css"
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/manager-dashboard" element={<ManagerDashboard />} />
+      <Route path="/menuboard" element={<MenuBoard />} />
     </Routes>
+    </ChakraProvider>
   );
 }
 
