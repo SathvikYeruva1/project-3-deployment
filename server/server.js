1291850e44dc60a46a9fd5ad5f6e2fa02f46be46
Kevtom2003 <kevtom2003@gmail.com>
2023-11-07 09:53:23 -0600

    added build

--- server/server.js
+++ server/server.js
@@ -1,5 +1,20 @@
 const express = require("express")
 const app = express()
+const path = require('path')
+const _dirname = path.dirname(__filename)
+const buildPath = path.join(_dirname, "../client/build")
+
+app.use(express.static(buildPath))
+app.get("/*",function(req,res){
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function(err){
+      if(err){
+        res.status(500).send(err);
+      }
+    }
+  );
+})
 
 app.get("/api", (req, res) => {
   res.json({"response": ["response1", "response2"]})
