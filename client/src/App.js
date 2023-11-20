b52da6d341e0f54c3ac168867df84dfaaea5398c
Kevtom2003 <kevtom2003@gmail.com>
2023-11-19 22:47:57 -0600

    added tables and pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Login from "./components/Login/Login";
 import ManagerDashboard from "./components/ManagerDashboard/ManagerDashboard";
 import MenuBoard from "./components/MenuBoard/menuboard";
+import Inventory from "./components/Inventory/inventory";
+import Employees from "./components/Employees/employees";
+import Menuinfo from "./components/Menuinfo/menuinfo";
 // import "./App.css"
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
@@ -15,6 +18,9 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/manager-dashboard" element={<ManagerDashboard />} />
       <Route path="/menuboard" element={<MenuBoard />} />
+      <Route path="/inventory" element={<Inventory />} />
+      <Route path="/employees" element={<Employees />} />
+      <Route path="/menuinfo" element={<Menuinfo />} />
     </Routes>
     </ChakraProvider>
   );
