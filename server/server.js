ccaff8e336aaaac8ddec2ddf4db5fa64365dbcaa
SathvikYeruva <saths.y@gmail.com>
2023-11-21 17:29:12 -0600

    added get for employee data to server

--- server/server.js
+++ server/server.js
@@ -92,6 +92,15 @@ app.get('/ordersdata', (req, res) => {
   });
 });
 
+//get all of the employees data
+app.get('/employeesdata', (req, res) => {
+  pool.query('SELECT * FROM employees;').then(query_res => {
+    res.json(query_res.rows);
+  }).catch(err => {
+    res.status(500).json({error: err.message});
+  });
+});
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(buildPath, 'index.html'), function (err) {
     if (err) {
