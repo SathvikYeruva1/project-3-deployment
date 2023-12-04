488c23bb499f7746a569e9317a9e05fcbfc30cb4
Kevtom2003 <kevtom2003@gmail.com>
2023-12-04 10:57:33 -0600

    testing page nav

--- client/src/components/ManagerDashboard/ManagerDashboard.js
+++ client/src/components/ManagerDashboard/ManagerDashboard.js
@@ -41,6 +41,19 @@ function ManagerDashboard() {
   const handleLogout = () => {
     navigate("/");
   };
+  const handleEmployee = () => {
+    navigate("/employees");
+  };
+  const handleInventory = () => {
+    navigate("/inventory");
+  };
+  const handleMenuInfo = () => {
+    navigate("/menuinfo");
+  };
+  const handleDashboard = () => {
+    navigate("/manager-dashboard");
+  };
+
 
   const TableRow = ({ data, borderBottom = true }) => (
     <Tr>
@@ -75,18 +88,18 @@ function ManagerDashboard() {
         />
         <UnorderedList styleType="none" p="0">
           <ListItem mb="15px" fontSize="lg">
-            <a href="/manager-dashboard">Dashboard</a>
+            <a onClick={handleDashboard}>Dashboard</a>
           </ListItem>
           <ListItem mb="15px" fontSize="lg">
-            <a href="/inventory">Inventory</a>
+            <a onClick={handleInventory}>Inventory</a>
           </ListItem>
           <ListItem mb="15px" fontSize="lg">
-            <a href = "/menuinfo">
+            <a onClick={handleMenuInfo}>
               Menu
             </a>
           </ListItem>
           <ListItem mb="15px" fontSize="lg">
-            <a href="/employees">Employees</a>
+            <a onClick={handleEmployee}>Employees</a>
           </ListItem>
           <ListItem mb="15px" fontSize="lg">
             <a href="/" onClick={handleLogout}>
